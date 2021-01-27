5f55af7552fa058896374573041a12a8ad3a1556
leanaelle.salmier <leanaelle.salmier@epitech.eu>
2021-01-27 22:09:57 +0100

    Creation form in a controller - Done

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,16 +9,15 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
-
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends AbstractController {
 
     /**
      * @Route("/user")
      */
-    function createUserForm() {
+    function createUserForm(Request $request) {
         $user = new User();
         $form = $this->createFormBuilder($user)
             ->add('name', TextType::class)
@@ -26,6 +25,13 @@ class UserController extends AbstractController {
             ->add('date', DateType::class)
             ->add('save', SubmitType::class)
             ->getform();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            return new Response("Formulaire valide.");
+        }
+        
         return $this->render('form.html.twig', ['form' => $form->createView()]);
     }
 
